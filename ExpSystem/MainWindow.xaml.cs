649c2c51c706db9b15bee62a39eb474255ab2d0c
Gertsog <asgertsog@gmail.com>
2018-10-14 11:33:21 +0600

    initial commit

---
+++ ExpSystem/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpSystem
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void GetDropFile(object sender, MouseButtonEventArgs e)
+        {
+            DragDrop.DoDragDrop(Grid, Grid.Resources, DragDropEffects.Copy);
+        }
+    }
+}
